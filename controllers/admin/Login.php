d2737b43b51094ff0594db7b1e8b1eea65dc2161
minhchou <88535002+minhchou@users.noreply.github.com>
2024-09-17 09:56:21 +0700

    Add files via upload

---
+++ controllers/admin/Login.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends MY_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('admin_model');
+		$this->load->library('form_validation');
+		$this->load->helper('form');
+	}
+
+	public function index()
+	{
+		$message_success = $this->session->flashdata('message_success');
+		$this->data['message_success'] = $message_success;
+
+		$message_fail = $this->session->flashdata('message_fail');
+		$this->data['message_fail'] = $message_fail;
+
+		if ($this->input->post()) {
+			$this->form_validation->set_rules('email', 'Email đăng nhập', 'required|valid_email');
+			$this->form_validation->set_rules('password', 'Mật khẩu', 'required');
+			$this->form_validation->set_rules('login', 'login', 'callback_check_login');
+
+			if ($this->form_validation->run())
+			{
+				$user = $this->get_info_login();
+				$this->session->set_userdata('login', $user);
+				$this->session->set_flashdata('message_success', "Đăng nhập thành công");
+				redirect(admin_url('home'));
+			}
+		}
+
+		$this->load->view('admin/login/index.php', $this->data);
+	}
+
+	public function check_login()
+	{
+		$user = $this->get_info_login();
+		return true;
+		if ($user) {
+			return true;
+		} else {
+			$this->form_validation->set_message(__FUNCTION__, 'Đăng nhập thất bại');
+			return false;
+		}
+		
+	
+	}
+
+	function get_info_login()
+	{
+		$email = $this->input->post("email");
+		$password = $this->input->post("password");
+
+		$where = array(
+			'email' => $email,
+			'password' => md5($password)
+		);
+
+		$user = $this->admin_model->get_info_rule($where);
+
+		if ($user) {
+			return $user;
+		} else {
+			return false;
+		}
+	}
+}
+?>
---
+++ views/admin/header.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span>Shop JUNO </span>Admin</a>
+            
+        </div>
+
+    </div><!-- /.container-fluid -->
+</nav>
+<script>
+   
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+</script>
